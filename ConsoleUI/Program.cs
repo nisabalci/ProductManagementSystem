5ad7e01958cca01ee8887451457a0785efccb80f
nisabalci <nisabalci00@gmail.com>
2021-02-13 14:57:14 +0300

    first commit

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleUI
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
